77e799b74fdc3a3a2e09e207d008b9706c055b7c
sk2111 <sathisht1996@gmail.com>
2021-05-29 19:27:07 +0530

    feat : User Info : Add user information
    Signed-off-by: sk2111 <sathisht1996@gmail.com>

--- js/index.js
+++ js/index.js
@@ -1,13 +1,17 @@
-import { updateNavigationBar } from './utilities/helpers.js';
+import {
+    updateNavigationBar, updateUserView, fetchJson, updateTitleCount, getUserList
+} from './utilities/helpers.js';
 
 //DOM nodes
 const searchViewNode = document.getElementById('search-view');
 const infoViewNode = document.getElementById('info-view');
+const totalCountNode = document.getElementById('total-search-count');
+const searchResultsNode = document.getElementById('search-results');
 const navItemsNode = document.querySelectorAll('.nav-item');
 const repoCountNode = document.getElementById('repo-count');
 const userCountNode = document.getElementById('user-count');
 
-//App constants
+//constants
 const NAV_SELECTED_CLASS = 'nav-selected';
 const NAV_REPOSITORIES = 'repositories';
 const NAV_USERS = 'users';
@@ -16,12 +20,20 @@ let currentNavView = NAV_REPOSITORIES;
 //variables
 let debounceId = null;
 let searchTerm = '';
+let usersPagination = 1;
+
+//API end points
+const GITHUB_USERS = 'https://api.github.com/search/users?per_page=10';
 
 
-
-const handleContentView = (currentNavView, searchTerm) => {
+const handleContentView = async (currentNavView, searchTerm) => {
     if (currentNavView === NAV_REPOSITORIES) {
-
+        const url = `${GITHUB_USERS}&q=${searchTerm}&page=${usersPagination}`;
+        const { total_count: totalCount, items: usersList } = await fetchJson(url);
+        updateTitleCount(totalCount, 'users', userCountNode, totalCountNode);
+        const userNodeList = getUserList(usersList);
+        searchResultsNode.innerHTML = '';
+        searchResultsNode.append(...userNodeList);
     }
     if (currentNavView === NAV_USERS) {
         //fetch data 
@@ -51,11 +63,8 @@ window.handleSearch = (e) => {
     }
     debounceId = setTimeout(() => {
         if (searchTerm.length) {
-            searchViewNode.style.visibility = 'visible';
-            infoViewNode.style.display = 'none';
-            return handleContentView(currentNavView, searchTerm);
+            handleContentView(currentNavView, searchTerm);
         }
-        infoViewNode.style.display = '';
-        searchViewNode.style.visibility = 'hidden';
+        updateUserView(searchTerm, searchViewNode, infoViewNode);
     }, 750);
 };
--- js/utilities/helpers.js
+++ js/utilities/helpers.js
@@ -1,4 +1,54 @@
+export const createDOMNode = (tagName, content, attribute = []) => {
+    const elem = document.createElement(tagName);
+    if (content) {
+        elem.innerText = content;
+    }
+    if (Array.isArray(attribute)) {
+        attribute.forEach(({ name, value }) => {
+            elem.setAttribute(name, value);
+        });
+    }
+    return elem;
+};
+
 export const updateNavigationBar = (eve, navItemsNode, selectedClass) => {
     navItemsNode.forEach(node => node.classList.remove(selectedClass));
     eve.classList.add(selectedClass);
+};
+
+export const updateUserView = (searchTerm, searchViewNode, infoViewNode) => {
+    const isValid = searchTerm.length;
+    searchViewNode.style.visibility = isValid ? 'visible' : 'hidden';
+    infoViewNode.style.display = isValid ? 'none' : '';
+};
+
+export const fetchJson = async (url) => {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        return data;
+    }
+    catch (e) {
+        console.log(e);
+        return [];
+    }
+};
+
+export const updateTitleCount = (totalCount, type, navNode, totalCountNode) => {
+    const total = Number(totalCount);
+    if (total) {
+        totalCountNode.innerText = `${total} ${type} results`;
+        navNode.innerText = total > 1000 ? `${parseInt(total / 1000)} k` : total;
+    }
+};
+
+export const getUserList = (userList) => {
+    const nodeList = userList.map(({ avatar_url, login, url }) => {
+        const listNode = createDOMNode('li', null, [{ name: 'class', value: 'user-item' }]);
+        const imgNode = createDOMNode('img', null, [{ name: 'src', value: avatar_url }, { name: 'class', value: 'user-avatar' }]);
+        const nameNode = createDOMNode('p', login, [{ name: 'class', value: 'user-name' }]);
+        listNode.append(imgNode, nameNode);
+        return listNode;
+    });
+    return nodeList;
 };
